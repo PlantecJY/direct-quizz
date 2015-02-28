b5d7f6ae807fe1e19c3daf6970c9b1400929e635
orosens@gmail.com <orosens@gmail.com@5ccb5866-7e30-2933-0d18-4d378e2ab8e6>
2015-02-28 18:39:37 +0000

    lien de vérification fonctionnel

---
+++ trunk/DirectQuiz/src/java/mails/SMTPManager.java
@@ -0,0 +1,101 @@
+package mails;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Classe gérant l'envoi de mail à travers le serveur SMTP.
+ *
+ * @author mohamedsqualli
+ */
+import java.util.Properties;
+import javax.activation.CommandMap;
+import javax.activation.MailcapCommandMap;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SMTPManager {
+
+    public static String mailUtil = "noreply.directquizz@gmail.com";
+    public static String passUtil = "quizz123**";
+
+    public static String generate() {
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; // Tu supprimes les lettres dont tu ne veux pas
+        String pass = "";
+        for (int x = 0; x < 10; x++) {
+            int i = (int) Math.floor(Math.random() * 62); // Si tu supprimes des lettres tu diminues ce nb
+            pass += chars.charAt(i);
+        }
+        System.out.println(pass);
+        return pass;
+    }
+
+    /**
+     * Envoi d'une confirmation lors de la création d'un compte à l'adresse
+     * "address" avec le mot de passe "mdp"
+     */
+    public static void sendCreateConfirmation(String adresse, String name, String code) {
+        String lien="http://localhost:8080/DirectQuiz/verif?name="+name+"&code="+code;
+        String mess = "Bonjour,\n\n" + "Votre compte sur l'application DirectQuizz est en cours de création.\nAfin de terminer le processus de création, veuillez suivre le lien suivant :\n\n"+lien+"\n\nA bientôt sur DirectQuizz : \n\n Note : Ce mail est un mail automatique, veuillez ne pas y répondre. Pour tout problème ou réclamations, adressez vous à un administrateur : mhaidir.ayoub@gmail.com";
+        String subject = "Création de votre compte sur DirectQuizz";
+        SMTPManager.sendMail(adresse, mess,subject);
+    }
+
+    /**
+     * Fonction générique pour envoyez un mail à l'adresse "address" dont le
+     * contenu est "msg" et le sujet "subject"
+     */
+    public static void sendMail(String address, String msg, String subject) {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(mailUtil, passUtil);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("noreply.carpooling.sopra@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(address));
+            message.setSubject(subject);
+            message.setText(msg);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Fonction utile uniquement pour les tests, n'est absolument pas utilisée
+     * lors du déroulement de l'application
+     */
+    public static void main(String[] args) {
+        //SMTPManager.sendMail("squallih@etud.insa-toulouse.fr", "Voici mon message,"
+        //        + "\n Hehe cocoo !", "Ca marche !");
+        
+    }
+}
--- trunk/DirectQuiz/src/java/servlets/Connexion.java
+++ trunk/DirectQuiz/src/java/servlets/Connexion.java
@@ -17,7 +17,8 @@ import forms.*;
 
 //@WebServlet( urlPatterns = { "/connexion" } )
 public class Connexion extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+
+    private static final long serialVersionUID = 1L;
 
     public static final String ATT_USER = "utilisateur";
     public static final String ATT_FORM = "form";
@@ -25,102 +26,100 @@ public class Connexion extends HttpServlet {
 
     // Injection de la façade Utilisateur (EJB Stateless)
     @EJB
-    private UtilisateurDao   utilisateurDao;
+    private UtilisateurDao utilisateurDao;
 
     @Override
-    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-                // récupération du paramètre
-		String action = request.getParameter("action");
-		// enregistrement
-		if(action.equals("enregistrement")){
-			String data = request.getParameter("data");
-			if(data.equals("false")){
-				// affichage du formulaire
-				RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_membre.jsp"); 
-				dispatch.forward(request, response);
-			} else {
-				// gestion des données du formulaire
-                                InscriptionForm gestionFormulaire = new InscriptionForm( utilisateurDao );
-                                // vérification des données de la requête / nouvel utilisateur
-                                Utilisateur utilisateur = gestionFormulaire.inscrireUtilisateur( request );
-                                // résultat du traitement et bean dans la requête
-                                request.setAttribute( ATT_FORM, gestionFormulaire );
-                                request.setAttribute( ATT_USER, utilisateur );
-                                // affichage du formulaire avec résultat de la soumission
-				RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_membre.jsp"); 
-				dispatch.forward(request, response);
-			}
-		} else if(action.equals("connexion")){
-                        // connexion
-			String data = request.getParameter("data");
-			if(data.equals("false")){
-				// affichage du formulaire
-				RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_connexion.jsp"); 
-				dispatch.forward(request, response);
-			} else {
-				// gestion des données du formulaire
-                                ConnexionForm gestionFormulaire = new ConnexionForm( utilisateurDao );
-                                // vérification des données de la requête / nouvel utilisateur
-                                Utilisateur utilisateur = gestionFormulaire.connecterUtilisateur( request );
-        		        // Récupération de la session depuis la requête
-        		        HttpSession session = request.getSession();
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // récupération du paramètre
+        String action = request.getParameter("action");
+        // enregistrement
+        if (action.equals("enregistrement")) {
+            String data = request.getParameter("data");
+            if (data.equals("false")) {
+                // affichage du formulaire
+                RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_membre.jsp");
+                dispatch.forward(request, response);
+            } else {
+                // gestion des données du formulaire
+                InscriptionForm gestionFormulaire = new InscriptionForm(utilisateurDao);
+                // vérification des données de la requête / nouvel utilisateur
+                Utilisateur utilisateur = gestionFormulaire.inscrireUtilisateur(request);
+                utilisateur.setValide(0);
+                // résultat du traitement et bean dans la requête
+                request.setAttribute(ATT_FORM, gestionFormulaire);
+                request.setAttribute(ATT_USER, utilisateur);
+                // affichage du formulaire avec résultat de la soumission
+                RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_membre.jsp");
+                dispatch.forward(request, response);
+            }
+        } else if (action.equals("connexion")) {
+            // connexion
+            String data = request.getParameter("data");
+            if (data.equals("false")) {
+                // affichage du formulaire
+                RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_connexion.jsp");
+                dispatch.forward(request, response);
+            } else {
+                // gestion des données du formulaire
+                ConnexionForm gestionFormulaire = new ConnexionForm(utilisateurDao);
+                // vérification des données de la requête / nouvel utilisateur
+                Utilisateur utilisateur = gestionFormulaire.connecterUtilisateur(request);
+                // Récupération de la session depuis la requête
+                HttpSession session = request.getSession();
         		        // si aucune erreur de validation n'a eu lieu, alors ajout du bean à la session :
-                                // la session peut être détruite dans plusieurs circonstances :
-                                //      l'utilisateur ferme son navigateur
-                                //      la session expire après une période d'inactivité de l'utilisateur
-                                // l'utilisateur se déconnecte.
-                                
-                                String page = "";
-        		        if ( utilisateur != null ) {
-        		            session.setAttribute( ATT_SESSION_USER, utilisateur );
-                                    page = "index.jsp";
-        		        } else {
-        		            session.setAttribute( ATT_SESSION_USER, null );
-                                    page = "formulaire_connexion.jsp";
-        		        }
-        		        // résultat du traitement et bean dans la requête
-        		        request.setAttribute( ATT_FORM, gestionFormulaire );
-        		        request.setAttribute( ATT_USER, utilisateur );
-                	        // affichage 
-				RequestDispatcher dispatch = request.getRequestDispatcher(page); 
- 				dispatch.forward(request, response);
-			}
-		} else if(action.equals("deconnexion")){
-        		// connexion
-			// Récupération et destruction de la session en cours 
-                        HttpSession session = request.getSession();
-                        session.invalidate();
-                        // Redirection
-                        //response.sendRedirect( URL_REDIRECTION );
- 			RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_connexion.jsp"); 
-			dispatch.forward(request, response);
-		} else if(action.equals("recuperer")){
-                        // récupération
-			String data = request.getParameter("data");
-			if(data.equals("false")){
-				// affichage du formulaire
-				RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_recuperation.jsp"); 
-				dispatch.forward(request, response);
-			} else {
-				// gestion des données du formulaire
-                                ConnexionForm gestionFormulaire = new ConnexionForm( utilisateurDao );
-                                // vérification des données de la requête / nouvel utilisateur
-                                Utilisateur utilisateur = gestionFormulaire.recupererUtilisateur( request );
-        		        request.setAttribute( ATT_FORM, gestionFormulaire );
-        		        request.setAttribute( ATT_USER, utilisateur );
-                            
-				RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_recuperation.jsp"); 
-				dispatch.forward(request, response);
-                        }
-                        
+                // la session peut être détruite dans plusieurs circonstances :
+                //      l'utilisateur ferme son navigateur
+                //      la session expire après une période d'inactivité de l'utilisateur
+                // l'utilisateur se déconnecte.
+
+                String page = "";
+                if (utilisateur != null&&utilisateur.getValide()==1) {
+                    session.setAttribute(ATT_SESSION_USER, utilisateur);
+                    page = "index.jsp";
+                } else {
+                    session.setAttribute(ATT_SESSION_USER, null);
+                    page = "formulaire_connexion.jsp";
                 }
+                // résultat du traitement et bean dans la requête
+                request.setAttribute(ATT_FORM, gestionFormulaire);
+                request.setAttribute(ATT_USER, utilisateur);
+                // affichage 
+                RequestDispatcher dispatch = request.getRequestDispatcher(page);
+                dispatch.forward(request, response);
+            }
+        } else if (action.equals("deconnexion")) {
+            // connexion
+            // Récupération et destruction de la session en cours 
+            HttpSession session = request.getSession();
+            session.invalidate();
+            // Redirection
+            //response.sendRedirect( URL_REDIRECTION );
+            RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_connexion.jsp");
+            dispatch.forward(request, response);
+        } else if (action.equals("recuperer")) {
+            // récupération
+            String data = request.getParameter("data");
+            if (data.equals("false")) {
+                // affichage du formulaire
+                RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_recuperation.jsp");
+                dispatch.forward(request, response);
+            } else {
+                // gestion des données du formulaire
+                ConnexionForm gestionFormulaire = new ConnexionForm(utilisateurDao);
+                // vérification des données de la requête / nouvel utilisateur
+                Utilisateur utilisateur = gestionFormulaire.recupererUtilisateur(request);
+                request.setAttribute(ATT_FORM, gestionFormulaire);
+                request.setAttribute(ATT_USER, utilisateur);
+
+                RequestDispatcher dispatch = request.getRequestDispatcher("formulaire_recuperation.jsp");
+                dispatch.forward(request, response);
+            }
+
+        }
     }
 
-    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-		doGet(request, response);
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
-    
+
 }
-
-
-       
